1c36996a622d9daa109e2cf8dbe8e2ee848f673f
princeraval147 <princeraval147@gmail.com>
2024-07-17 10:08:38 +0530

    Final Year Project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,13 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
 
 const Header = () => {
-
-    const Cars = [
-        'Hyundai',
-        'Toyota',
-        'Suzuki',
-        'Tata',
-    ]
     return (
         <>
             <div className="header">
-                <NavLink to='/'>CrazyCar</NavLink>
-                <Autocomplete
-                    disablePortal
-                    id=""
-                    options={Cars}
-                    sx={{ width: 250 }}
-                    renderInput={(params) => <TextField {...params} label="Cars" />}
-                />
+                <h1>
+                    <NavLink className='logo' to='/'>CrazyCar</NavLink>
+                </h1>
                 <ul className='links'>
                     <li>
                         <NavLink to='/' className='link'>Home</NavLink>
