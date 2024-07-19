75fcb45a2257f1888d274057651439ea4a6a0313
princeraval147 <princeraval147@gmail.com>
2024-07-19 09:45:28 +0530

    Final Year Project

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,49 +1,92 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
+
+    const Navigate = useNavigate();
+
+    const [input, setInput] = useState({
+        userName: "",
+        email: "",
+        password: "",
+        conformPassword: ""
+    });
+
+    const handlerSubmit = (e) => {
+        e.preventDefault();
+        console.table(input)
+        if (input.password === input.conformPassword) {
+            Navigate('/login');
+        }
+        else {
+            alert("Conform Password Can't Match");
+        }
+    }
+
+
     return (
         <>
-            <div class="sign">
-                <form class="form" action="">
+            <div className="sign">
+                <form className="form" action="" onSubmit={handlerSubmit}>
                     <h1>Create Your Account</h1>
-                    <label for="">Username : </label>
+                    <label htmlFor="userName">Username : </label>
                     <input
                         className='inputFeild'
                         type="text"
-                        name=""
-                        id=""
+                        name="userName"
+                        id="userName"
                         placeholder="Enter Your Name"
                         required
+                        value={input.userName}
+                        onChange={e => setInput({
+                            ...input,
+                            [e.target.name]: e.target.value
+                        })}
                     />
 
-                    <label for="">Email :</label>
+                    <label htmlFor="email">Email :</label>
                     <input
                         className='inputFeild'
                         type="email"
-                        name=""
-                        id=""
+                        name="email"
+                        id="email"
                         placeholder="Enter Email Address"
                         required
+                        value={input.email}
+                        onChange={e => setInput({
+                            ...input,
+                            [e.target.name]: e.target.value
+                        })}
                     />
 
-                    <label for="">Password :</label>
+                    <label htmlFor="password">Password :</label>
                     <input
                         className='inputFeild'
                         type="password"
-                        name=""
-                        id=""
+                        name="password"
+                        id="password"
                         placeholder="••••••••••••••••"
-                        required
+                        // required
+                        value={input.password}
+                        onChange={e => setInput({
+                            ...input,
+                            [e.target.name]: e.target.value
+                        })}
                     />
 
-                    <label for="">Confirm Password :</label>
+                    <label htmlFor="conformPassword">Confirm Password :</label>
                     <input
                         className='inputFeild'
                         type="password"
-                        name=""
-                        id=""
+                        name="conformPassword"
+                        id="conformPassword"
                         placeholder="Re-Enter Password"
-                        required
+                        // required
+                        value={input.conformPassword}
+                        onChange={e => setInput({
+                            ...input,
+                            [e.target.name]: e.target.value
+                        })}
                     />
                     <div>
                         <button type="submit">Sign Up</button>
