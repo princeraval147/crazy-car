d59b41d95cbc6dc73c5f869a203776ec2abff3ad
princeraval147 <princeraval147@gmail.com>
2024-08-13 22:05:11 +0530

    Backend Live

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -9,7 +9,7 @@ const Login = () => {
 
     const onSubmit = async (data) => {
         try {
-            const response = await fetch('http://localhost:5000/login', {
+            const response = await fetch('https://crazy-car.onrender.com/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -10,7 +10,7 @@ const SignUp = () => {
         console.log(data);
         if (data.password === data.conformPassword) {
             try {
-                const response = await fetch('http://localhost:5000/signup', {
+                const response = await fetch('https://crazy-car.onrender.com/signup', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
