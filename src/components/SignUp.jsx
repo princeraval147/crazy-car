e296a825fb3cb241f8418a65ea3bd15da53a936e
princeraval147 <princeraval147@gmail.com>
2024-08-14 12:46:30 +0530

    Final Year Project

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -10,7 +10,7 @@ const SignUp = () => {
         console.log(data);
         if (data.password === data.conformPassword) {
             try {
-                const response = await fetch('http://localhost:5000/contact', {
+                const response = await fetch('http://localhost:5000/signUp', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json'
