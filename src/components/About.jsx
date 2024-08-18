118c8e8b0b6bd207892793d3a8f80ce2607c16cf
princeraval147 <princeraval147@gmail.com>
2024-08-18 18:06:52 +0530

    About Page Done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,22 @@ import React from 'react'
 const About = () => {
     return (
         <>
-            <p style={{ height: "100vh" }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, magni dolorum suscipit voluptate dolore explicabo dolores nihil, esse veniam optio quas magnam iusto molestiae qui culpa cum reiciendis quo dolor.</p>
+            <div className="about">
+                <h2 className='aboutHeading'>
+                    About CrazyCar
+                </h2>
+                <p>
+                    crazycar.netlify.app is India's leading car search venture that helps users buy cars that are right for them. Its website and app carry rich automotive content such as expert reviews, detailed specs and prices, comparisons as well as videos and pictures of all car brands and models available in India. The company has tie-ups with many auto manufacturers, more than 4000 car dealers and numerous financial institutions to facilitate the purchase of vehicles.
+                    <br /><br />
+                    crazycar.netlify.app has launched many innovative features to ensure that users get an immersive experience of the car model before visiting a dealer showroom. These include a Feel The Car tool that gives 360-degree interior/exterior views with sounds of the car and explanations of features with videos; search and comparison by make, model, price, features; and live offers and promotions in all cities. The platform also has used car classifieds wherein users can upload their cars for sale, and find used cars for buying from individuals and used car dealers.
+                    <br /><br />
+                    Besides the above consumer product features, crazycar.netlify.app provides a rich array of tech-enabled tools to OE manufacturers and car dealers. These include apps for dealer sales executives to manage leads, cloud services for tracking sales performance, call tracker solution, digital marketing support, virtual online showroom and outsourced lead management operational process for taking consumers from enquiry to sale.
+                    <br /><br />
+                    Our vision is to construct a complete ecosystem for consumers and car manufacturers, dealers and related businesses such that consumers have easy and complete access to not only buying and selling cars, but also manage their entire ownership experience, be it accessories, tyres, batteries, insurance or roadside assistance.
+                    <br /><br />
+                    The company has expanded to Southeast Asia with the launch of Zigwheels.ph, Zigwheels.my and Oto.com. It also has a presence in the UAE with Zigwheels.ae
+                </p>
+            </div>
         </>
     )
 }
