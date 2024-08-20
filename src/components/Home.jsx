42bf0b1a3308ef378d2666a33d81df8de46492e5
princeraval147 <princeraval147@gmail.com>
2024-08-20 13:35:11 +0530

    Clean Design

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import styles from "./Design.module.css";
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
 import 'react-slideshow-image/dist/styles.css'
@@ -90,105 +89,106 @@ const Home = () => {
             </div>
 
             {/* <-------------------------------  Animation Content ------------------------------> */}
-            <div className={styles.container}>
-                <div className={styles.circles}>
-                    <div className={styles.mainCircle}>
-                        <div className={styles.innerCircleWrapper}>
-                            <div className={styles.innerCircleAnim} />
+
+            <div className="container">
+                <div className="circles">
+                    <div className="mainCircle">
+                        <div className="innerCircleWrapper">
+                            <div className="innerCircleAnim" />
                         </div>
-                        <div className={styles.firstCircle}>
-                            <div className={styles.dot1}></div>
+                        <div className="firstCircle">
+                            <div className="dot1"></div>
                         </div>
-                        <div className={styles.secondCircle}>
-                            <div className={styles.dot2}></div>
-                            <div className={styles.dot3}></div>
+                        <div className="secondCircle">
+                            <div className="dot2"></div>
+                            <div className="dot3"></div>
                         </div>
-                        <div className={styles.thirdCircle}>
-                            <div className={styles.dot4}></div>
+                        <div className="thirdCircle">
+                            <div className="dot4"></div>
                         </div>
 
-                        <div className={styles.fourthCircle}>
-                            <div className={styles.anii}>
-                                <p className={styles.animationText}>10+</p>
-                                <p className={styles.animationContent}>Years of Excellence</p>
+                        <div className="fourthCircle">
+                            <div className="anii">
+                                <p className="animationText">10+</p>
+                                <p className="animationContent">Years of Excellence</p>
                             </div>
-                            <div className={styles.anii}>
-                                <p className={styles.animationText} style={{ color: "#02BCD2" }}>
+                            <div className="anii">
+                                <p className="animationText" style={{ color: "#02BCD2" }}>
                                     100+
                                 </p>
-                                <p className={styles.animationContent}>Happy Clients</p>
+                                <p className="animationContent">Happy Clients</p>
                             </div>
-                            <div className={styles.anii}>
-                                <p className={styles.animationText} style={{ color: "#7ED602" }}>
+                            <div className="anii">
+                                <p className="animationText" style={{ color: "#7ED602" }}>
                                     25+
                                 </p>
-                                <p className={styles.animationContent}>CAR EXPERTS</p>
+                                <p className="animationContent">CAR EXPERTS</p>
                             </div>
                         </div>
                     </div>
                 </div>
 
                 <div style={{ marginTop: 0 }}>
-                    <div className={styles.titles}>
-                        <div className={styles.mainTitle}>INNOVATION</div>
-                        <div className={styles.both}>
-                            <div className={styles.centerTitle}>EXPERTISE EXCELLENCE</div>
-                            <div className={styles.lastTitle} style={{ marginLeft: 15 }}>
+                    <div className="titles">
+                        <div className="mainTitle">INNOVATION</div>
+                        <div className="both">
+                            <div className="centerTitle">EXPERTISE EXCELLENCE</div>
+                            <div className="lastTitle" style={{ marginLeft: 15 }}>
                                 RESULTS
                             </div>
                         </div>
                     </div>
-                    <div className={styles.mixDiv}>
-                        <div className={styles.Circles}>
-                            <div className={styles.mainCircle}>
-                                <div className={styles.innerCircleWrapper}>
-                                    <div className={styles.innerCircleAnim} />
+                    <div className="mixDiv">
+                        <div className="Circles">
+                            <div className="mainCircle">
+                                <div className="innerCircleWrapper">
+                                    <div className="innerCircleAnim" />
                                 </div>
-                                <div className={styles.firstCircle}>
-                                    <div className={styles.dot1}></div>
+                                <div className="firstCircle">
+                                    <div className="dot1"></div>
                                 </div>
-                                <div className={styles.secondCircle}>
-                                    <div className={styles.dot2}></div>
-                                    <div className={styles.dot3}></div>
+                                <div className="secondCircle">
+                                    <div className="dot2"></div>
+                                    <div className="dot3"></div>
                                 </div>
-                                <div className={styles.thirdCircle}>
-                                    <div className={styles.dot4}></div>
+                                <div className="thirdCircle">
+                                    <div className="dot4"></div>
                                 </div>
 
-                                <div className={styles.fourthCircle}>
-                                    <div className={styles.anii}>
-                                        <p className={styles.animationText}>10+</p>
-                                        <p className={styles.animationContent}>Years of Excellence</p>
+                                <div className="fourthCircle">
+                                    <div className="anii">
+                                        <p className="animationText">10+</p>
+                                        <p className="animationContent">Years of Excellence</p>
                                     </div>
-                                    <div className={styles.anii}>
+                                    <div className="anii">
                                         <p
-                                            className={styles.animationText}
+                                            className="animationText"
                                             style={{ color: "#02BCD2" }}
                                         >
                                             100+
                                         </p>
-                                        <p className={styles.animationContent}>Happy Clients</p>
+                                        <p className="animationContent">Happy Clients</p>
                                     </div>
-                                    <div className={styles.anii}>
+                                    <div className="anii">
                                         <p
-                                            className={styles.animationText}
+                                            className="animationText"
                                             style={{ color: "#7ED602" }}
                                         >
                                             25+
                                         </p>
-                                        <p className={styles.animationContent}>CAR EXPERTS</p>
+                                        <p className="animationContent">CAR EXPERTS</p>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div className={styles.desc}>
+                        <div className="desc">
                             <p>
                                 CrazyCar brings a complete range of new cars in 2024 in India with prices. You can search cars by applying filters such as by price, by bodytype, by brand, by seating capacity & more. Also, stay updated with upcoming cars, electric cars in India, compare cars in your price range.
                             </p>
                         </div>
                     </div>
                 </div>
-            </div>
+            </div >
         </>
     )
 }
