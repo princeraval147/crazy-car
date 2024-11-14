8e8d002e4c47a97d3bf8772ebbdeb55692dac1bb
princeraval147 <princeraval147@gmail.com>
2024-11-14 14:53:36 +0530

    Add Chart

--- src/components/Admin/CarRatingAnalysis.jsx
+++ src/components/Admin/CarRatingAnalysis.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { PieChart, Pie, Legend, Tooltip } from "recharts";
 
 function CarRatingAnalysis() {
     const [topCars, setTopCars] = useState([]);
@@ -10,6 +11,7 @@ function CarRatingAnalysis() {
                 const response = await fetch('http://localhost:5000/car-rating-analysis');
                 // const response = await fetch('https://crazycar-backend.onrender.com/car-rating-analysis');
                 const data = await response.json();
+                console.log(data);
                 setTopCars(data.topCars);
                 setCarRatingsWithPercentages(data.carRatingsWithPercentages);
             } catch (error) {
@@ -19,22 +21,48 @@ function CarRatingAnalysis() {
         fetchCarRatingAnalysis();
     }, []);
 
+    //  Chart
+    const data01 = [
+        { name: "Car A", value: 400 },
+        { name: "Car B", value: 300 },
+        { name: "Car C", value: 300 }
+    ];
+    const formattedTopCars = topCars.map((car) => ({
+        name: `${car.brand} ${car.model}`,
+        value: car.totalRatings,
+    }));
+
     return (
         <div className='RatingContainer'>
             <h1 align="center" className='cra'>Car Rating Analysis</h1>
-
-            <section className='topCarsSection'>
-                <h2>Top 3 Cars</h2>
-                <div className='topCarsContainer'>
-                    {topCars.map((car, index) => (
-                        <div key={car.carId} className='topCarItem'>
-                            <h3>{index + 1}. {car.brand} {car.model}</h3>
-                            <p>Average Rating: {car.averageRating.toFixed(1)} / 5</p>
-                            <p>Total Ratings: {car.totalRatings}</p>
-                        </div>
-                    ))}
-                </div>
-            </section>
+            <h2 align="center">Top 3 Cars</h2>
+            <div className="topCars">
+                <PieChart width={350} height={400} className='PieChart'>
+                    <Pie
+                        dataKey="value"
+                        isAnimationActive={false}
+                        // data={data01}
+                        data={formattedTopCars}
+                        cx={200}
+                        cy={200}
+                        outerRadius={80}
+                        fill="#8884d8"
+                        label
+                    />
+                    <Tooltip />
+                </PieChart>
+                <section className='topCarsSection'>
+                    <div className='topCarsContainer'>
+                        {topCars.map((car, index) => (
+                            <div key={car.carId} className='topCarItem'>
+                                <h3>{index + 1}. {car.brand} {car.model}</h3>
+                                <p>Average Rating: {car.averageRating.toFixed(1)} / 5</p>
+                                <p>Total Ratings: {car.totalRatings}</p>
+                            </div>
+                        ))}
+                    </div>
+                </section>
+            </div>
 
             <section className='allCarsSection'>
                 <h2>All Cars Rating in Percentage</h2>
