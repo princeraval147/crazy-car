1e3cd1f5b632cfa446d1e5f239aec27156fa90a8
Prince <Prince>
2024-12-05 19:04:07 +0530

    Formate Document

--- src/components/Admin/Addcar.jsx
+++ src/components/Admin/Addcar.jsx
@@ -1,13 +1,19 @@
-import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
-import '../../index.css';
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import "../../index.css";
 
 const Addcar = () => {
-  const { register, handleSubmit, setError, clearErrors, formState: { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    setError,
+    clearErrors,
+    formState: { errors },
+  } = useForm();
   const Navigate = useNavigate();
   const [modelExists, setModelExists] = useState(false);
-  const [priceUnit, setPriceUnit] = useState('Lakhs');
+  const [priceUnit, setPriceUnit] = useState("Lakhs");
 
   // Check if model exists
   const checkModelAvailability = async (model) => {
@@ -16,10 +22,10 @@ const Addcar = () => {
       // const response = await fetch(`https://crazycar-backend.onrender.com/checkmodel/${model}`);
       const result = await response.json();
       if (response.ok && result.exists) {
-        setError('model', { type: 'manual', message: 'Model already exists' });
+        setError("model", { type: "manual", message: "Model already exists" });
         setModelExists(true);
       } else {
-        clearErrors('model');
+        clearErrors("model");
         setModelExists(false);
       }
     } catch (error) {
@@ -28,7 +34,7 @@ const Addcar = () => {
   };
 
   const preventMinus = (e) => {
-    if (e.code === 'Minus') {
+    if (e.code === "Minus") {
       e.preventDefault();
     }
   };
@@ -38,18 +44,18 @@ const Addcar = () => {
     data.price = `${data.price} ${priceUnit}`;
 
     try {
-      const response = await fetch('http://localhost:5000/cardata', {
+      const response = await fetch("http://localhost:5000/cardata", {
         // const response = await fetch('https://crazycar-backend.onrender.com/cardata', {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
       });
       const result = await response.json();
       if (response.ok) {
-        alert('Car Added Successfully');
-        Navigate('/car');
+        alert("Car Added Successfully");
+        Navigate("/car");
       } else {
         alert(result.message);
       }
@@ -60,34 +66,39 @@ const Addcar = () => {
 
   return (
     <div className="add-car-form">
-      <h1 className='addCarHeading'>Add Car</h1>
+      <h1 className="addCarHeading">Add Car</h1>
       <form onSubmit={handleSubmit(onSubmit)} className="car-form">
         <div className="form-left">
-          <label>Model:
+          <label>
+            Model:
             <input
               type="text"
-              {...register('model', { required: 'Model is required' })}
+              {...register("model", { required: "Model is required" })}
               className="inputFeild"
               onBlur={(e) => checkModelAvailability(e.target.value)}
             />
-            {modelExists && <span className='errorMsg'>{errors.model?.message}</span>}
+            {modelExists && (
+              <span className="errorMsg">{errors.model?.message}</span>
+            )}
           </label>
 
-          <label>Brand:
+          <label>
+            Brand:
             <input
               type="text"
-              {...register('brand', { required: 'Brand is required' })}
+              {...register("brand", { required: "Brand is required" })}
               className="inputFeild"
             />
           </label>
 
-          <label>Price:
+          <label>
+            Price:
             <input
               type="number"
               step={0.01}
-              {...register('price', {
-                required: 'Price is required',
-                min: { value: 3, message: 'Price cannot be negative' }
+              {...register("price", {
+                required: "Price is required",
+                min: { value: 3, message: "Price cannot be negative" },
               })}
               className="inputFeild"
               onKeyDown={preventMinus}
@@ -101,116 +112,212 @@ const Addcar = () => {
               <option value="Lakhs">Lakhs</option>
               <option value="Crores">Crores</option>
             </select>
-            {errors.price && <span style={{ color: 'red' }}>{errors.price.message}</span>}
+            {errors.price && (
+              <span style={{ color: "red" }}>{errors.price.message}</span>
+            )}
           </label>
 
-          <label>Year:
+          <label>
+            Year:
             <input
               type="number"
-              {...register('year', {
-                required: 'Year is required',
-                min: { value: 1900, message: 'Year must be greater than 1900' },
-                max: { value: new Date().getFullYear(), message: `Year cannot be beyond ${new Date().getFullYear()}` }
+              {...register("year", {
+                required: "Year is required",
+                min: { value: 1900, message: "Year must be greater than 1900" },
+                max: {
+                  value: new Date().getFullYear(),
+                  message: `Year cannot be beyond ${new Date().getFullYear()}`,
+                },
               })}
               className="inputFeild"
               min={1900}
             />
-            {errors.year && <span style={{ color: 'red' }}>{errors.year.message}</span>}
+            {errors.year && (
+              <span style={{ color: "red" }}>{errors.year.message}</span>
+            )}
           </label>
 
           <label>Fuel Type:</label>
           <div>
             <label>
-              <input type="radio" value="Electric" {...register('fuelType', { required: 'Fuel type is required' })} />
+              <input
+                type="radio"
+                value="Electric"
+                {...register("fuelType", { required: "Fuel type is required" })}
+              />
               Electric
             </label>
-            <label style={{ marginLeft: '15px' }}>
-              <input type="radio" value="Petrol" {...register('fuelType', { required: 'Fuel type is required' })} />
+            <label style={{ marginLeft: "15px" }}>
+              <input
+                type="radio"
+                value="Petrol"
+                {...register("fuelType", { required: "Fuel type is required" })}
+              />
               Petrol
             </label>
-            <label style={{ marginLeft: '15px' }}>
-              <input type="radio" value="Diesel" {...register('fuelType', { required: 'Fuel type is required' })} />
+            <label style={{ marginLeft: "15px" }}>
+              <input
+                type="radio"
+                value="Diesel"
+                {...register("fuelType", { required: "Fuel type is required" })}
+              />
               Diesel
             </label>
           </div>
-          {errors.fuelType && <span style={{ color: 'red' }}>{errors.fuelType.message}</span>}
+          {errors.fuelType && (
+            <span style={{ color: "red" }}>{errors.fuelType.message}</span>
+          )}
 
-          <br></br><label>Transmission:</label>
+          <br></br>
+          <label>Transmission:</label>
           <div>
             <label>
-              <input type="radio" value="Automatic" {...register('transmission', { required: 'Transmission is required' })} />
+              <input
+                type="radio"
+                value="Automatic"
+                {...register("transmission", {
+                  required: "Transmission is required",
+                })}
+              />
               Automatic
             </label>
-            <label style={{ marginLeft: '15px' }}>
-              <input type="radio" value="Manual" {...register('transmission', { required: 'Transmission is required' })} />
+            <label style={{ marginLeft: "15px" }}>
+              <input
+                type="radio"
+                value="Manual"
+                {...register("transmission", {
+                  required: "Transmission is required",
+                })}
+              />
               Manual
             </label>
           </div>
-          {errors.transmission && <span style={{ color: 'red' }}>{errors.transmission.message}</span>}
-          <br></br><label>Seating Capacity:</label>
+          {errors.transmission && (
+            <span style={{ color: "red" }}>{errors.transmission.message}</span>
+          )}
+          <br></br>
+          <label>Seating Capacity:</label>
           <div>
             <label>
-              <input type="radio" value="5" {...register('seatingCapacity', { required: 'Seating capacity is required' })} />
+              <input
+                type="radio"
+                value="5"
+                {...register("seatingCapacity", {
+                  required: "Seating capacity is required",
+                })}
+              />
               5
             </label>
-            <label style={{ marginLeft: '15px' }}>
-              <input type="radio" value="7" {...register('seatingCapacity', { required: 'Seating capacity is required' })} />
+            <label style={{ marginLeft: "15px" }}>
+              <input
+                type="radio"
+                value="7"
+                {...register("seatingCapacity", {
+                  required: "Seating capacity is required",
+                })}
+              />
               7
             </label>
-            <br></br><label>Body Type:
-              <input type="text" {...register('bodyType', { required: 'Body type is required' })} className="inputFeild" />
+            <br></br>
+            <label>
+              Body Type:
+              <input
+                type="text"
+                {...register("bodyType", { required: "Body type is required" })}
+                className="inputFeild"
+              />
             </label>
           </div>
-          {errors.seatingCapacity && <span style={{ color: 'red' }}>{errors.seatingCapacity.message}</span>}
-
+          {errors.seatingCapacity && (
+            <span style={{ color: "red" }}>
+              {errors.seatingCapacity.message}
+            </span>
+          )}
         </div>
 
-
         <div className="form-right">
-
-          <label>Mileage (km/l):
-            <input type="text" {...register('mileage', { required: 'Mileage is required' })} className="inputFeild" />
-          </label>
-
-          <label>Engine Capacity (cc):
+          <label>
+            Mileage (km/l):
             <input
               type="text"
-              {...register('engineCapacity')}
+              {...register("mileage", { required: "Mileage is required" })}
               className="inputFeild"
             />
           </label>
 
-          <label>Boot Space (liters):
+          <label>
+            Engine Capacity (cc):
+            <input
+              type="text"
+              {...register("engineCapacity")}
+              className="inputFeild"
+            />
+          </label>
+
+          <label>
+            Boot Space (liters):
             <input
               type="number"
-              {...register('bootSpace', {
-                required: 'Boot space is required',
-                min: { value: 0, message: 'Boot space cannot be negative' }
+              {...register("bootSpace", {
+                required: "Boot space is required",
+                min: { value: 0, message: "Boot space cannot be negative" },
               })}
               className="inputFeild"
             />
-            {errors.bootSpace && <span style={{ color: 'red' }}>{errors.bootSpace.message}</span>}
+            {errors.bootSpace && (
+              <span style={{ color: "red" }}>{errors.bootSpace.message}</span>
+            )}
           </label>
 
-          <label>Safety Features:
-            <input type="text" {...register('safetyFeatures', { required: 'Safety features are required' })} className="inputFeild" />
+          <label>
+            Safety Features:
+            <input
+              type="text"
+              {...register("safetyFeatures", {
+                required: "Safety features are required",
+              })}
+              className="inputFeild"
+            />
           </label>
-          <label>Features:
-            <input type="text" {...register('features', { required: 'Features are required' })} className="inputFeild" />
+          <label>
+            Features:
+            <input
+              type="text"
+              {...register("features", { required: "Features are required" })}
+              className="inputFeild"
+            />
           </label>
-          <label>Warranty:
-            <input type="text" {...register('warranty', { required: 'Warranty is required' })} className="inputFeild" />
+          <label>
+            Warranty:
+            <input
+              type="text"
+              {...register("warranty", { required: "Warranty is required" })}
+              className="inputFeild"
+            />
           </label>
-          <label>Description:
-            <textarea {...register('description', { required: 'Description is required' })} className="inputFeild textarea-input" />
+          <label>
+            Description:
+            <textarea
+              {...register("description", {
+                required: "Description is required",
+              })}
+              className="inputFeild textarea-input"
+            />
           </label>
-          <label>Image URL:
-            <input type="text" {...register('image', { required: 'Image URL is required' })} className="inputFeild" />
+          <label>
+            Image URL:
+            <input
+              type="text"
+              {...register("image", { required: "Image URL is required" })}
+              className="inputFeild"
+            />
           </label>
         </div>
 
         <div className="form-button-container">
-          <button type="submit" className="form-button">Add Car</button>
+          <button type="submit" className="form-button">
+            Add Car
+          </button>
         </div>
       </form>
     </div>
@@ -218,4 +325,3 @@ const Addcar = () => {
 };
 
 export default Addcar;
-
