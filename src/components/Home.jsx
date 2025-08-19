b14f8956df8c0a8d076da056da42c0ec32528089
princeraval147 <princeraval147@gmail.com>
2025-08-20 00:16:22 +0530

    Add Top Cars

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -34,11 +34,26 @@ const Home = () => {
     ];
 
 
+    const [topCars, setTopCars] = useState([]);
+    const fetchCarRatingAnalysis = async () => {
+        try {
+            const response = await fetch(
+                "http://localhost:5000/car-rating-analysis"
+            );
+            const data = await response.json();
+            setTopCars(data.topCars);
+        } catch (error) {
+            console.error("Error fetching car rating analysis:", error);
+        }
+    };
+    useEffect(() => {
+        fetchCarRatingAnalysis();
+    }, []);
+
 
     return (
         <>
             {/* SlideShow */}
-
             <div className="slide-container">
                 <Slide>
                     {slideImages.map((slideImage, index) => (
@@ -51,12 +66,20 @@ const Home = () => {
                 </Slide>
             </div>
 
-            {/* ------------------------------------------------ */}
+            {/* ------------------------------------------------------------------------- */}
             <div className="homepage-content">
                 <section className="featured-cars">
                     <h2>Featured Cars This Week</h2>
                     <div className="car-grid">
-                        <div className="car-card">
+                        {
+                            topCars.map((car) => (
+                                <div key={car.carId} className='car-card'>
+                                    <h3>{car.brand} {car.model}</h3>
+                                    <p>₹{car.price} - {car.year} Model</p>
+                                </div>
+                            ))
+                        }
+                        {/* <div className="car-card">
                             <h3>🚗 Tata Nexon EV</h3>
                             <p>₹14.7L – 2024 Model</p>
                         </div>
@@ -67,7 +90,7 @@ const Home = () => {
                         <div className="car-card">
                             <h3>🚘 Maruti Fronx</h3>
                             <p>₹9.5L – 2024 Model</p>
-                        </div>
+                        </div> */}
                     </div>
                     <NavLink to="/car" className="view-all-btn">View All Cars →</NavLink>
                 </section>
